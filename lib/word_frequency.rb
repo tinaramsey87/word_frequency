49ba2699a216878f118a6d639a17af71625f698c
Tina Ramsey <tina.ramsey87@gmail.com>
2015-01-09 14:21:52 -0800

    It will attempt to delte punctutation...does not work yet

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,12 +1,33 @@
 class String
   define_method(:word_frequency) do |search_word|
-    search_array = self.upcase().split(" ")
+
     search_word.upcase!()
-#binding.pry
-        if search_array.include?(search_word)
-           search_array.count(search_word)
+
+    search_array = self.split(" ")
+    string_to_clean = search_array.join("")
+    clean_search_string = string_to_clean.delete_punctuation()
+
+    # search_array = self.upcase().split(" ")
+
+    clean_search_array = clean_search_string.upcase().split(" ")
+# binding.pry
+        if clean_search_array.include?(search_word)
+           clean_search_array.count(search_word)
         else
           "That word is not found in the phrase"
         end
   end
+
+  define_method(:delete_punctuation) do
+    punctuation = [",", ".", "!", "?", "-", "&", ";", ":", "/", "(", ")", "*", "@"]
+    letters_array = self.split("")
+    punctuation.each() do |punc|
+      letters_array.each() do |letter|
+        letters_array.delete(punc)
+# binding.pry
+      end
+    end
+    letters_array.join("")
+  end
+
 end
